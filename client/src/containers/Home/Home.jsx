dd00ee6a345fc948aca9852a8e82dfcea33119aa
Fedex159 <fede.gallina94@gmail.com>
2022-01-17 19:53:25 -0300

    home load top movie and popular

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import NavBar from "./NavBar/NavBar";
+import TopMovie from "./TopMovie/TopMovie";
+import PopularMovies from "./PopularMovies/PolularMovies";
 import s from "./Home.module.css";
 
 function Home() {
   return (
     <div className={s.container}>
       <NavBar />
+      <div className={s.movies}>
+        <TopMovie title="La casa de papel" subtitle="Original de liteflix" />
+        <PopularMovies />
+      </div>
     </div>
   );
 }
