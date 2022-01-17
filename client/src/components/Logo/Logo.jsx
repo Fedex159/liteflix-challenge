7927b7966f97517096b49a7787c530f47b34e589
Fedex159 <fede.gallina94@gmail.com>
2022-01-17 15:22:00 -0300

    add logo component

---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./Logo.module.css";
+
+function Logo() {
+  return (
+    <div className={s.container}>
+      <h2>
+        <strong>Lite</strong>flix
+      </h2>
+    </div>
+  );
+}
+
+export default Logo;
