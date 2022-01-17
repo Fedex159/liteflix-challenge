76dcc26c10dbb259499f7381c6ca34589eeb1cc0
Fedex159 <fede.gallina94@gmail.com>
2022-01-17 15:57:32 -0300

    load navbar in home

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import NavBar from "./NavBar/NavBar";
 import s from "./Home.module.css";
 
 function Home() {
-  return <div className={s.container}>Home</div>;
+  return (
+    <div className={s.container}>
+      <NavBar />
+    </div>
+  );
 }
 
 export default Home;
