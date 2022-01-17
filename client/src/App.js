9a9fbd80c8100eefd137686c65ec1d3e91d0aff5
Fedex159 <fede.gallina94@gmail.com>
2022-01-17 15:07:31 -0300

    load home a set size app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Liteflix</h1>
+      <Home />
     </div>
   );
 }
