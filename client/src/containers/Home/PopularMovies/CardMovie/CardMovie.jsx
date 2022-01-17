cb0d7e2ebed9459579c3881726adfdb35c6f051d
Fedex159 <fede.gallina94@gmail.com>
2022-01-17 19:51:36 -0300

    cardmovie create and config

---
+++ client/src/containers/Home/PopularMovies/CardMovie/CardMovie.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Star from "../../../../assets/imgs/Star.png";
+import s from "./CardMovie.module.css";
+
+// ‣
+function CardMovie({ title, img, rating, year }) {
+  return (
+    <div className={s.container} style={{ backgroundImage: `url(${img})` }}>
+      <div className={s.play}></div>
+      <h4 className={s.title}>{title}</h4>
+      <div className={s.infoHover}>
+        <div>
+          <img src={Star} alt="star" />
+          <h5>{rating}</h5>
+        </div>
+        <h5>{year}</h5>
+      </div>
+    </div>
+  );
+}
+
+export default CardMovie;
