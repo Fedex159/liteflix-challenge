0437c8bc3e02b24be761f0df1286c297fd1a4e95
Fedex159 <fede.gallina94@gmail.com>
2022-01-18 19:11:59 -0300

    upload message component create and config

---
+++ client/src/containers/Home/NavBar/AddMovie/ModalUploadMovie/UploadMessage/UploadMessage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Logo from "../../../../../../components/Logo/Logo";
+import cerrar from "../../../../../../assets/imgs/cerrar.png";
+import s from "./UploadMessage.module.css";
+
+function UploadMessage({ title, onClick }) {
+  return (
+    <div className={s.container}>
+      <button className={s.close} onClick={onClick}>
+        <img src={cerrar} alt="cerrar" />
+      </button>
+      <Logo />
+      <h4 className={s.title}>¡Felicitaciones!</h4>
+      <h5 className={s.subtitle}>{title} fue correctamente subida.</h5>
+      <button className={s.buttonHome} onClick={onClick}>
+        Ir a home
+      </button>
+    </div>
+  );
+}
+
+export default UploadMessage;
