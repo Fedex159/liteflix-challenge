669ebe85f3da66923e92545bf79715c5f88847eb
Fedex159 <fede.gallina94@gmail.com>
2022-01-18 19:32:43 -0300

    load modal in add movie button

--- client/src/containers/Home/NavBar/AddMovie/AddMovie.jsx
+++ client/src/containers/Home/NavBar/AddMovie/AddMovie.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import plus from "../../../../assets/imgs/plus.png";
+import ModalUploadMovie from "./ModalUploadMovie/ModalUploadMovie";
 import s from "./AddMovie.module.css";
 
 function AddMovie() {
+  const [showAddMovie, setShowAddMovie] = useState(false);
+
+  const handleModal = () => {
+    setShowAddMovie((prev) => !prev);
+  };
+
   return (
     <div className={s.container}>
       <img src={plus} alt="plus" />
-      <button>Agregar Película</button>
+      <button className={s.addMovie} onClick={handleModal}>
+        Agregar Película
+      </button>
+      {showAddMovie ? <ModalUploadMovie handleModal={handleModal} /> : null}
     </div>
   );
 }
