53ce0197b70ea92a5a363798c45cfe43c0f2371a
Fedex159 <fede.gallina94@gmail.com>
2022-01-18 10:03:44 -0300

    create route /movie/:type

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,8 @@
-const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
+const { Router } = require("express");
+const movies = require("./movies");
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
+router.use("/movies", movies);
 
 module.exports = router;
