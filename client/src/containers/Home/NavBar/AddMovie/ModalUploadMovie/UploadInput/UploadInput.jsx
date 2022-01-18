5ddd96edf8fc7099b54c9e9f4f319e19d941f9e1
Fedex159 <fede.gallina94@gmail.com>
2022-01-18 18:42:44 -0300

    Upload input component create and config

---
+++ client/src/containers/Home/NavBar/AddMovie/ModalUploadMovie/UploadInput/UploadInput.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import clip from "../../../../../../assets/imgs/clip.png";
+import s from "./UploadInput.module.css";
+
+function UploadInput({ handleDrop, handleInputFile }) {
+  return (
+    <div
+      className={s.container}
+      onDrop={handleDrop}
+      onDragOver={(event) => event.preventDefault()}
+    >
+      <img src={clip} alt="clip" />
+      <p>
+        <strong>Agregá un archivo</strong> o arrastralo y soltalo aquí
+      </p>
+      <input
+        type="file"
+        name="file"
+        className={s.inputFile}
+        onChange={handleInputFile}
+      />
+    </div>
+  );
+}
+
+export default UploadInput;
