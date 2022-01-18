b3272f4f9fa6e1a3b25b8991c9da512d021e9a3a
Fedex159 <fede.gallina94@gmail.com>
2022-01-18 10:29:05 -0300

    load top movie when start home

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar/NavBar";
 import TopMovie from "./TopMovie/TopMovie";
 import PopularMovies from "./PopularMovies/PolularMovies";
+import { getMovies } from "../../utils";
 import s from "./Home.module.css";
 
 function Home() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    getMovies("now_playing")
+      .then((data) => setMovie(data))
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
-    <div className={s.container}>
+    <div
+      className={s.container}
+      style={{ backgroundImage: `url(${movie.img})` }}
+    >
       <NavBar />
       <div className={s.movies}>
-        <TopMovie title="La casa de papel" subtitle="Original de liteflix" />
+        <TopMovie title={movie.title} />
         <PopularMovies />
       </div>
     </div>
