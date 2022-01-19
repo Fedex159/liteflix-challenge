6ddad33dbf4912ef1d361ac126d73e52ab85e155
Fedex159 <fede.gallina94@gmail.com>
2022-01-19 15:18:58 -0300

    config client for deployment

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export async function getMovies(type) {
-  const { data } = await axios.get(`http://localhost:3001/movies/${type}`);
+  const { data } = await axios.get(`/movies/${type}`);
   return data;
 }
 
 export async function addMovieToDB(movie) {
-  const { data } = await axios.post("http://localhost:3001/movies", movie);
+  const { data } = await axios.post("/movies", movie);
   return data;
 }
