e1c9adb1b651c4503c7897bb8190555f05ccf54b
Fedex159 <fede.gallina94@gmail.com>
2022-01-19 10:03:48 -0300

    add utils for add movie to db

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,3 +4,8 @@ export async function getMovies(type) {
   const { data } = await axios.get(`http://localhost:3001/movies/${type}`);
   return data;
 }
+
+export async function addMovieToDB(movie) {
+  const { data } = await axios.post("http://localhost:3001/movies", movie);
+  return data;
+}
