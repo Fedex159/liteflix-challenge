79e95cbe4f9f1d7f0514b4dd84eb176c678ea2a1
Fedex159 <fede.gallina94@gmail.com>
2022-01-19 11:50:09 -0300

    add strong to subtitule

--- client/src/containers/Home/TopMovie/TopMovie.jsx
+++ client/src/containers/Home/TopMovie/TopMovie.jsx
@@ -6,7 +6,9 @@ import s from "./TopMovie.module.css";
 function TopMovie({ title }) {
   return (
     <div className={s.container}>
-      <h4 className={s.subtitle}>Original de liteflix</h4>
+      <h4 className={s.subtitle}>
+        Original de <strong>liteflix</strong>
+      </h4>
       <h2 className={s.title}>{title}</h2>
       <div className={s.buttons}>
         <ButtonPlay />
