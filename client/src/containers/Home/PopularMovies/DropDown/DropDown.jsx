2b002b4354290f4d8864ec366ac5c652bb36d584
Fedex159 <fede.gallina94@gmail.com>
2022-01-19 11:18:54 -0300

    load context in dropdown and boxdrop

--- client/src/containers/Home/PopularMovies/DropDown/DropDown.jsx
+++ client/src/containers/Home/PopularMovies/DropDown/DropDown.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import arrow from "../../../../assets/imgs/arrow.png";
+import BoxDrop from "./BoxDrop/BoxDrop";
+import { StateGlobal } from "../../Home";
 import s from "./DropDown.module.css";
 
-function DropDown({ option }) {
+function DropDown() {
+  const [showBoxDrop, setShowBoxDrop] = useState(false);
+  const { option } = useContext(StateGlobal);
+
   return (
     <div className={s.container}>
-      <button>
+      <button
+        className={s.buttonOption}
+        onClick={() => setShowBoxDrop((prev) => !prev)}
+      >
         <h4>
           VER: <strong>{option}</strong>
         </h4>
         <img src={arrow} alt="arrow" />
       </button>
+      {showBoxDrop ? <BoxDrop /> : null}
     </div>
   );
 }
