85500d6f8a0a18eb5e6d25b17cc056ffe3057968
Fedex159 <fede.gallina94@gmail.com>
2022-01-19 13:23:51 -0300

    load button play and change styles

--- client/src/containers/Home/MoviesContainer/CardMovie/CardMovie.jsx
+++ client/src/containers/Home/MoviesContainer/CardMovie/CardMovie.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import ButtonPlay from "./ButtonPlay/ButtonPlay";
 import Star from "../../../../assets/imgs/Star.png";
 import s from "./CardMovie.module.css";
 
 function CardMovie({ title, img, ranking, year }) {
   return (
     <div className={s.container} style={{ backgroundImage: `url(${img})` }}>
-      <div className={s.play}></div>
+      <div className={s.play}>
+        <ButtonPlay />
+      </div>
       <h4 className={s.title}>{title}</h4>
       <div className={s.infoHover}>
-        <div>
-          <img src={Star} alt="star" />
-          <h5>{ranking}</h5>
-        </div>
-        <h5>{year}</h5>
+        {ranking ? (
+          <div>
+            <img src={Star} alt="star" />
+            <h5>{ranking}</h5>
+          </div>
+        ) : null}
+        {year ? <h5>{year}</h5> : null}
       </div>
     </div>
   );
