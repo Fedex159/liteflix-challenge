ac005a34a143f24f178d3c85531f3502236f720a
Fedex159 <fede.gallina94@gmail.com>
2022-01-20 11:40:40 -0300

    replace img menu for menu icon component

--- client/src/containers/Home/NavBar/NavItems/NavItems.jsx
+++ client/src/containers/Home/NavBar/NavItems/NavItems.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Menu from "../../../../assets/imgs/Menú.png";
 import Notificación from "../../../../assets/imgs/Notificación.png";
 import Perfil from "../../../../assets/imgs/Perfil.png";
+import MenuIcon from "../../../../components/MenuIcon/MenuIcon";
 import s from "./NavItems.module.css";
 
 function NavItems() {
   return (
     <div className={s.container}>
-      <img className={s.menu} src={Menu} alt="Menu" />
+      <div className={s.menu}>
+        <MenuIcon />
+      </div>
       <img className={s.notificacion} src={Notificación} alt="Notificación" />
       <img className={s.perfil} src={Perfil} alt="Perfil" />
     </div>
