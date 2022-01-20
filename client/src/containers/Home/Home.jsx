23e0bffa8bdf7de6f0d428aa6c96f1d4e727de26
Fedex159 <fede.gallina94@gmail.com>
2022-01-20 17:50:17 -0300

    add animation zoom to background

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -16,6 +16,8 @@ function Home() {
   const [myMovies, setMyMovies] = useState([]);
   const [option, setOption] = useState(options[0]);
   const [showModal, setShowModal] = useState(false);
+  const [imageSize, setImageSize] = useState(100);
+  const [img, setImg] = useState(null);
 
   const handleModal = () => {
     setShowModal((prev) => !prev);
@@ -24,7 +26,13 @@ function Home() {
   useEffect(() => {
     // Top movie
     getMovies("now_playing")
-      .then((data) => setTopMovie(data))
+      .then((data) => {
+        setTopMovie(data);
+
+        const img = new Image();
+        img.src = data.img;
+        setImg(img);
+      })
       .catch((e) => console.log("now_playing", e));
 
     // Popular movies
@@ -50,6 +58,37 @@ function Home() {
     }
   }, [ref, showModal]);
 
+  // For animation background
+  useEffect(() => {
+    if (ref.current && img) {
+      const handleResize = () => {
+        const topMoviesRef = ref.current.children[1].children[0];
+
+        const width =
+          ref.current.clientWidth > 900
+            ? ref.current.clientWidth
+            : topMoviesRef.clientWidth;
+        const height =
+          width > 900 ? ref.current.clientHeight : topMoviesRef.clientHeight;
+
+        const ratio = width / height;
+
+        if (ratio < 16 / 9) {
+          setImageSize(Math.ceil(((16 / 9) * 100) / ratio));
+        } else {
+          setImageSize(100);
+        }
+      };
+      handleResize();
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, [ref, img]);
+
   return (
     <StateGlobal.Provider
       value={{
@@ -65,7 +104,11 @@ function Home() {
       <div
         ref={ref}
         className={s.container}
-        style={{ "--url": `url(${topMovie.img})` }}
+        style={{
+          "--url": `url(${topMovie.img})`,
+          "--size": `${imageSize}%`,
+          backgroundSize: `${imageSize}%`,
+        }}
       >
         <NavBar />
         <div className={s.movies}>
