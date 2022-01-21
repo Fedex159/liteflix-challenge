c6363d1424764a96250124de87d06ebf8de6cdaa
Fedex159 <fede.gallina94@gmail.com>
2022-01-21 10:25:31 -0300

    move context to index

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Context from "./containers/Context/Context";
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>,
   document.getElementById("root")
 );
