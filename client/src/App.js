f3519738de6e5885327cebef7a05b522f530ee02
Fedex159 <fede.gallina94@gmail.com>
2022-01-21 10:25:25 -0300

    laod spiner

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
-import "./App.css";
-import Context from "./containers/Context/Context";
+import React, { useContext } from "react";
 import Home from "./containers/Home/Home";
+import Loading from "./components/Loading/Loading";
+import { StateGlobal } from "./containers/Context/Context";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Context>
-        <Home />
-      </Context>
-    </div>
-  );
+  const { isLoading } = useContext(StateGlobal);
+  return <div className="App">{isLoading ? <Loading /> : <Home />}</div>;
 }
 
 export default App;
