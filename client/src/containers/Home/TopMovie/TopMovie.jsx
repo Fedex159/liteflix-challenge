d817b90004180d7ac4138b34d759200caafec1af
Fedex159 <fede.gallina94@gmail.com>
2022-01-22 10:48:37 -0300

    remove background image and load component

--- client/src/containers/Home/TopMovie/TopMovie.jsx
+++ client/src/containers/Home/TopMovie/TopMovie.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import ButtonPlay from "./ButtonPlay/ButtonPlay";
 import ButtonAddList from "./ButtonAddList/ButtonAddList";
+import BackgroundImg from "../../../components/BackgroundImg/BackgroundImg";
+import { StateGlobal } from "../../Context/Context";
 import s from "./TopMovie.module.css";
 
-function TopMovie({ title }) {
+function TopMovie({ title, img }) {
+  const { screenSize } = useContext(StateGlobal);
   return (
     <div className={s.container}>
+      {screenSize.width <= 900 || screenSize.height <= 720 ? (
+        <BackgroundImg src={img} />
+      ) : null}
       <h4 className={s.subtitle}>
         Original de <strong>liteflix</strong>
       </h4>
